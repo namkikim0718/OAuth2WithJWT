ac2800ef498bd858e6658985eff01e034fa3da0d
namkikim0718 <113903598+namkikim0718@users.noreply.github.com>
2024-05-29 17:28:25 +0900

    init: Initialize Project

---
+++ src/main/java/com/be/OAuth2WithJWT/OAuth2WithJwtApplication.java
@@ -0,0 +1,13 @@
+package com.be.OAuth2WithJWT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OAuth2WithJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OAuth2WithJwtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/be/OAuth2WithJWT/OAuth2WithJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.be.OAuth2WithJWT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OAuth2WithJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
